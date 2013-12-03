4b21db039a6e1c6260edd3ad4229a2668c875203
JuliaYa <julia.yakutova@gmail.com>
2013-12-03 18:16:15 +0400

    create pdf with results

--- get_rss.rb
+++ get_rss.rb
@@ -1,14 +1,39 @@
 require 'open-uri'
 require 'nokogiri'
+require "prawn"
 
-def get_habr_rss
-	#doc = Nokogiri::HTML(open("http://habrahabr.ru/"))
-	#RSS_URL = 'http://habrahabr.ru/'
-	#PROXY = ???
-	#open('http://tigerslife.com/')
-	rss = Nokogiri::HTML(open('http://tigerslife.com/'))
-	titles = rss.search('h2')
-	puts titles
+
+def get_rss url
+	rss = Nokogiri::XML(open(url))
+	titles = rss.search('item/title')
+	# puts titles
+
+	Prawn::Document.generate("rss.pdf") do
+	  data = ''
+	  titles.each do |node|
+		   data = data + node.inner_text + "\n"
+	  end
+	  text data
+	end
+	puts 'success!'
 end
 
-get_habr_rss()
+def get_habr_rss
+	get_rss ('http://habrahabr.ru/rss/feed/posts/4345c787866eca0f1e6e2977b29bf275/')
+end
+
+def get_tigerslive_rss
+	get_rss ('http://tigerslife.com/feed/')
+end
+
+def get_tigromania_rss
+	get_rss ('http://tigromania.ru/rss.xml')
+end
+
+# get_habr_rss()
+#get_tigerslive_rss()
+#get_tigromania_rss()
+str = ARGV[0]
+get_rss (str)
+
+# $ ruby ./get_rss.rb "http://egghead.io/feed"
