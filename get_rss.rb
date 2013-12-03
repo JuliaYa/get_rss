a4dc8d657fa3ce0e7b58f72fdb0fe6e8fb3f1513
JuliaYa <julia.yakutova@gmail.com>
2013-12-03 19:18:45 +0400

    Output settable count of posts and descriptions

--- get_rss.rb
+++ get_rss.rb
@@ -3,37 +3,49 @@ require 'nokogiri'
 require "prawn"
 
 
-def get_rss url
+def get_rss(url, count)
 	rss = Nokogiri::XML(open(url))
 	titles = rss.search('item/title')
-	# puts titles
+	contents = rss.search('item/description')
+
+	data = ''
+	counter = 0
+
+	if titles.length < count
+		count = titles.length
+	end
+
+	titles[0..count].each do |node|
+	   data = data + node.inner_text + "\n\n"
+	   data = data + contents[counter].inner_text + "\n\n\n"
+	   counter = counter + 1
+	end
 
 	Prawn::Document.generate("rss.pdf") do
-	  data = ''
-	  titles.each do |node|
-		   data = data + node.inner_text + "\n"
-	  end
 	  text data
 	end
+
 	puts 'success!'
 end
 
-def get_habr_rss
-	get_rss ('http://habrahabr.ru/rss/feed/posts/4345c787866eca0f1e6e2977b29bf275/')
+def get_habr_rss # don't work, need autorisation
+	get_rss('http://habrahabr.ru/rss/feed/posts/4345c787866eca0f1e6e2977b29bf275/')
 end
 
 def get_tigerslive_rss
-	get_rss ('http://tigerslife.com/feed/')
+	get_rss('http://tigerslife.com/feed/', 5)
 end
 
 def get_tigromania_rss
-	get_rss ('http://tigromania.ru/rss.xml')
+	get_rss('http://tigromania.ru/rss.xml', 5)
 end
 
 # get_habr_rss()
-#get_tigerslive_rss()
-#get_tigromania_rss()
-str = ARGV[0]
-get_rss (str)
+# get_tigerslive_rss()
+# get_tigromania_rss()
 
-# $ ruby ./get_rss.rb "http://egghead.io/feed"
+get_rss(ARGV.shift, ARGV.shift.to_i)
+
+# get_rss("http://egghead.io/feed", 5)
+
+# $ ruby ./get_rss.rb "http://egghead.io/feed" 5
